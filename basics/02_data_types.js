ae704028e59a257f835012f6e0369d4f5ef12501
ananyasvyas <74373484+ananyasvyas@users.noreply.github.com>
2023-12-08 07:02:17 +0000

    use strict statement, ecma script, premitive data types, typeof operator

---
+++ basics/02_data_types.js
@@ -0,0 +1,18 @@
+"use strict"; // treats code as newer version of js, by default, it is in strict mode only
+
+// ecma script - defining standards for js
+
+// Premitive data types -
+// number - 2^53
+// string - ''/""
+// boolean - true/false
+// null - standalone value/representation of empty value
+// undefined - Variabled declared but not assigned
+// symbol - used to identify uniqueness
+// bigint
+
+// use typeof to identify the type of any variable
+let age = 4;
+typeof(age); // number
+typeof(null) // object
+typeof(undefined) // undefined
