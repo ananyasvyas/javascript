e9cf656b7296259b220ddc5940bf452da25783df
ananyasvyas <74373484+ananyasvyas@users.noreply.github.com>
2023-12-08 06:23:28 +0000

    JS vaiable declaration, assignment, reassignment

---
+++ basics/variable_declaration.js
@@ -0,0 +1,6 @@
+// variable declaration
+const id = 86723; // not allowed to be reassigned.
+let email = 'vyasananyashekhar@gmail.com'; // allows block scope.
+var password = 'qw1234r'; // var does not have block/functional scope, its value can get changed anywhere it is updated.
+city = 'Udaipur'; // variables declared without any keyword becomes global variable, irrespective of where they are declared.
+let state; // has default value as undefined
