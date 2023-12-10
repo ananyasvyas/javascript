e8c37020d6171d99b7093b93ba7be3e7ba1b14b8
ananyasvyas <74373484+ananyasvyas@users.noreply.github.com>
2023-12-10 11:35:47 +0000

    truthy and falsy values

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -2,3 +2,7 @@
 console.log("2" == 2); // true
 // check value and data type
 console.log("2" === 2); // false
+// falsy vaues
+// false, 0, -0, "", null, undefined, NaN
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
