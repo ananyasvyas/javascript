293491038de797e050270b0cc69d9b401d2ae655
ananyasvyas <74373484+ananyasvyas@users.noreply.github.com>
2023-12-10 08:46:54 +0000

    Arrow function, implicit and explict return

---
+++ basics/10_arrow_functions.js
@@ -0,0 +1,9 @@
+// arrow function
+() => {}
+const arrFunc = () => {}
+
+// returning value from arrow function
+(num) => { // explicit return
+    return num; 
+}
+(num) => (num); // implicit return
