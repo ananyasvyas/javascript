790250eda964bcf6e60a5468e93f78f01725c4a7
ananyasvyas <74373484+ananyasvyas@users.noreply.github.com>
2023-12-10 11:42:19 +0000

    Nullish Coalescing Operator (??)

--- 01_basics/07_operators.js
+++ 01_basics/07_operators.js
@@ -3,3 +3,6 @@ function calculateMarks(...marks) {
     return marks;
 }
 console.log(calculateMarks(200, 300, 100, 400)); // [200, 300, 100, 400]
+// Nullish Coalescing Operator (??) selects between two values; if first is null or undefined, selects the second value
+const val = 5 ?? 10; // 5
+const val1 = null ?? 10; // 10
