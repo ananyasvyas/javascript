8833e8766099e7f32bc183828c2eed5262fa390a
Ananya Shekhar Vyas <74373484+ananyasvyas@users.noreply.github.com>
2023-12-16 12:00:14 +0000

    'new' keyword

--- 02_advance/06_oop.js
+++ 02_advance/06_oop.js
@@ -9,12 +9,15 @@ const user = {
 }
 
 // constructor function
-// 'new' is constructor function, which helps to create multiple instances of same object literal
-// new can be used to create instances of functions
-function createUser(username, isValid) { // this constructor function gets convereted to a class declaration
+// 'new' is constructor function, which helps to create multiple instances of same object literal. new can be used to create instances of functions
+// The new keyword initiates the creation of a new JavaScript object.
+// The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructors prototype.
+// The constructor function is called with the specified arguments and gets bound to the newly created object. If nor explicit return value is specified from the contructor, JS assumes 'this' (newly created object), to be the intended return value.
+function createUser(username, isValid, marks) { // this constructor function gets convereted to a class declaration
     this.username = username;
     this.isValid = isValid;
+    this.marks = marks;
     return this; // this is by-default returned, but included to increase readability
 }
-console.log(new createUser('ananya', true));
-console.log(new createUser('shekhar', false));
+console.log(new createUser('ananya', true, 55));
+console.log(new createUser('shekhar', false, 78));
